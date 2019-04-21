9a76ab3870951f00c24d3763f613e7af42bd20e3
Himanshu Patel <himanshup@csu.fullerton.edu>
2019-04-21 06:35:26 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Geolocation from "react-geolocation";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Geolocation
+          render={({
+            fetchingPosition,
+            position: { coords: { latitude, longitude } = {} } = {},
+            error,
+            getCurrentPosition
+          }) => (
+            <div>
+              <button onClick={getCurrentPosition}>Get Position</button>
+              {error && <div>{error.message}</div>}
+              <pre>
+                latitude: {latitude}
+                longitude: {longitude}
+              </pre>
+            </div>
+          )}
+        />
       </div>
     );
   }
